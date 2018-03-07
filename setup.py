e1913b6cb970e8c13b32976afab6944f92ab2a9d
Ivar Matstoms <ivar.matstoms@gmail.com>
2018-03-07 22:26:00 +0100

    added setuptools

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+setup(
+    name="GAMLP",
+    version="0.1",
+    packages=find_packages(),
+    scripts=[],
+    package_data={
+    },
+
+    author="ALI",
+    author_email="ivar.matstoms@gmail.com",
+    description="Math lib in pure python",
+    license="GPLv3",
+    keywords="math",
+    project_urls={
+    }
+)
